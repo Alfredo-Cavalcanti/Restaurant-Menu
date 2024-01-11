d752aa4fd7205c1ba532ca5180d98607971ccf5f
Alfredo-Cavalcanti <139989159+Alfredo-Cavalcanti@users.noreply.github.com>
2024-01-11 18:06:45 -0300

    Estilização e semi-posicionamento do botão darkMode

--- src/components/Botao/index.jsx
+++ src/components/Botao/index.jsx
@@ -4,9 +4,13 @@ import styles from "../../styles/Home.module.css"; // Seu arquivo de estilos
 
 export default function Botao({ toggleDarkMode, darkMode }) {
   return (
-    <div>
+    <div className={styles.darkModeButtonContainer}>
       {/* Botão de alternância */}
-      <button onClick={toggleDarkMode} className={darkMode ? styles.darkMode : ''}>
+      <button
+        onClick={toggleDarkMode}
+        className={`${styles.darkModeButton} ${darkMode ? styles.darkMode : ''}`}
+        style={{ backgroundColor: darkMode ? 'white' : '' }}
+      >
         {darkMode ? "Modo Claro" : "Modo Escuro"}
       </button>
     </div>
