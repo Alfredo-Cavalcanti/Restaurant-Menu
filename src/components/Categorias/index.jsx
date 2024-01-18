931061604bfba8e1f96393deff225fe5f8d0fa8e
Alfredo-Cavalcanti <139989159+Alfredo-Cavalcanti@users.noreply.github.com>
2024-01-18 18:31:40 -0300

    Refatoracao do componente categorias, movendo os dados para a pasta data e reaproveitando o componente

--- src/components/Categorias/index.jsx
+++ src/components/Categorias/index.jsx
@@ -2,46 +2,18 @@ import styles from "./Categorias.module.css";
 
 import Image from "next/image";
 
-import icone1 from "../../../public/assets/entrada.png";
-import icone2 from "../../../public/assets/massa.png";
-import icone3 from "../../../public/assets/carne.png";
-import icone4 from "../../../public/assets/bebidas.png";
-import icone5 from "../../../public/assets/salada.png";
-import icone6 from "../../../public/assets/sobremesa.png";
+import { botoes } from "@/data/data-categorias";
 
 const Categorias = ({ handleFiltro, botaoClicado, darkMode }) => {
 	return (
 		<section className={styles.secao_categorias}>
 			<div className={`${styles.container_botoes} ${darkMode ? styles.darkMode : ""}`}>
-				<button className={botaoClicado === "Appetizers" ? styles.acenderBtn : styles.apagarBtn} onClick={() => handleFiltro("Appetizers")}>
-					<Image className={styles.icone} src={icone1} alt="ícone" />
-					Appetizers
-				</button>
-
-				<button className={botaoClicado === "Pastas" ? styles.acenderBtn : styles.apagarBtn} onClick={() => handleFiltro("Pastas")}>
-					<Image className={styles.icone} src={icone2} alt="ícone" />
-					Pastas
-				</button>
-
-				<button className={botaoClicado === "Meats" ? styles.acenderBtn : styles.apagarBtn} onClick={() => handleFiltro("Meats")}>
-					<Image className={styles.icone} src={icone3} alt="ícone" />
-					Meats
-				</button>
-
-				<button className={botaoClicado === "Drinks" ? styles.acenderBtn : styles.apagarBtn} onClick={() => handleFiltro("Drinks")}>
-					<Image className={styles.icone} src={icone4} alt="ícone" />
-					Drinks
-				</button>
-
-				<button className={botaoClicado === "Salads" ? styles.acenderBtn : styles.apagarBtn} onClick={() => handleFiltro("Salads")}>
-					<Image className={styles.icone} src={icone5} alt="ícone" />
-					Salads
-				</button>
-
-				<button className={botaoClicado === "Desserts" ? styles.acenderBtn : styles.apagarBtn} onClick={() => handleFiltro("Desserts")}>
-					<Image className={styles.icone} src={icone6} alt="ícone" />
-					Desserts
-				</button>
+				{botoes.map((botao) => (
+					<button className={botaoClicado === botao.nome ? styles.acenderBtn : styles.apagarBtn} onClick={() => handleFiltro(botao.nome)}>
+						<Image className={styles.icone} src={botao.icone} alt="ícone" />
+						{botao.nome}
+					</button>
+				))}
 			</div>
 		</section>
 	);
---
+++ src/data/data-categorias.js
@@ -0,0 +1,39 @@
+import icone1 from "../../public/assets/entrada.png"
+import icone2 from "../../public/assets/massa.png";
+import icone3 from "../../public/assets/carne.png";
+import icone4 from "../../public/assets/bebidas.png";
+import icone5 from "../../public/assets/salada.png";
+import icone6 from "../../public/assets/sobremesa.png";
+
+export const botoes = [
+	{
+		id: 1,
+		nome: "Appetizers",
+		icone: icone1,
+	},
+	{
+		id: 2,
+		nome: "Pastas",
+		icone: icone2,
+	},
+	{
+		id: 3,
+		nome: "Meats",
+		icone: icone3,
+	},
+	{
+		id: 4,
+		nome: "Drinks",
+		icone: icone4,
+	},
+	{
+		id: 5,
+		nome: "Salads",
+		icone: icone5,
+	},
+	{
+		id: 6,
+		nome: "Desserts",
+		icone: icone6,
+	},
+];
